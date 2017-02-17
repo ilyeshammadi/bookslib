0e11457595687903c935f56edbaf495727cd4fc7
Ilyes Hammadi <hammadiilyesahmed@gmail.com>
2017-02-17 21:27:25 +0100

    feat:	Add tags to model Article

--- books_library/books/models.py
+++ books_library/books/models.py
@@ -3,11 +3,16 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+from taggit.managers import TaggableManager
+
+
 class Book(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField()
     link_to_pdf = models.URLField()
     thumbnail = models.ImageField(upload_to='books-thumbnail/', default='books-thumbnail/default.jpg')
+
+    tags = TaggableManager()
 
     def __str__(self):
         return self.title
--- config/settings/base.py
+++ config/settings/base.py
@@ -52,6 +52,7 @@ THIRD_PARTY_APPS = [
     'allauth',  # registration
     'allauth.account',  # registration
     'allauth.socialaccount',  # registration
+    'taggit', # Tags management
 ]
 
 # Apps specific for this project go here.
