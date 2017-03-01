af87e4eca7125fee9c5950158045c328bd9854f9
Ilyes Hammadi <hammadiilyesahmed@gmail.com>
2017-03-01 02:27:27 +0100

    feat:	Add the new Book model fields to the Book Form

--- books_library/books/forms.py
+++ books_library/books/forms.py
@@ -5,4 +5,5 @@ from .models import Book
 class BookForm(forms.ModelForm):
     class Meta:
         model = Book
-        fields = ['name', 'author', 'description', 'link_to_pdf', 'thumbnail', 'tags', 'categories']
+        fields = ['name', 'author', 'description', 'link_to_pdf', 'thumbnail', 'publication_date', 'publication_place',
+                  'language','tags', 'categories']
