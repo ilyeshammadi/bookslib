81ef81470d4a938c62fd0c665bbc1093188dd7de
Ilyes Hammadi <hammadiilyesahmed@gmail.com>
2017-05-14 13:22:20 +0100

    feat: Add Twitter API auth

---
+++ books_library/static/js/account/login.js
@@ -0,0 +1,14 @@
+/**
+ * Created by ilyes on 5/14/17.
+ */
+$(document).ready(function () {
+
+    // Setup twitter
+    $('.twitter').addClass('btn btn-primary');
+    $('.twitter').prepend('<i class="social-icon fa fa-twitter"></i>');
+
+    // Setup Google plus
+    
+    // Setup Facebook
+
+});
