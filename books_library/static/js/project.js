b613ba927fd59ddef4022a729b08f31ef6eb29a9
Ilyes Hammadi <hammadiilyesahmed@gmail.com>
2017-05-26 14:08:33 +0100

    feat: Add realtime search bar results

--- books_library/static/js/project.js
+++ books_library/static/js/project.js
@@ -19,3 +19,77 @@
  4. Undocumented: No mention in the documentation, or it's too hard for me to find
  */
 $('.form-group').removeClass('row');
+
+
+let xhr = null;
+const $searchBar = $('#search-bar');
+const $searchResults = $('#search-results');
+
+function closeResultList() {
+    $searchBar.removeClass('open');
+    $searchResults.html('');
+}
+
+
+function openResultList() {
+    $searchResults.css({
+        width: '30vw'
+    });
+
+    $searchBar.addClass('open')
+}
+
+function search(searchTerms) {
+    // Stop any prevoius AJAX call
+    if (xhr !== null) {
+        xhr.abort();
+        xhr = null;
+    }
+
+
+    // Search terms is empty close the search result list
+    if (!searchTerms.length > 0) {
+        console.log('Empty');
+        closeResultList();
+    } else {
+        xhr = $.ajax({
+            type: "GET",
+            url: "/api/books_search",
+            data: "search=" + searchTerms,
+            success: function (data) {
+                // Open dropdown
+                if (!$searchBar.hasClass('open')) {
+                    openResultList();
+                }
+
+                // Clean the search results from prevouis data
+                $searchResults.html('');
+
+                // If there are results show them
+                // Otherwise close the list
+                if (data.results.length > 0) {
+                    data.results.forEach(function (item) {
+                        // Append data here
+                        $searchResults.append('<li><a href="/books/detail/' + item.slug + '">' + item.name + '</a></li>')
+                    })
+                } else {
+                    closeResultList();
+                }
+            }
+        });
+    }
+
+
+}
+
+
+$("#search-bar").find("input").on('change paste keyup', function () {
+    const searchTerms = $(this).val();
+    console.log(searchTerms);
+
+    setTimeout(function () {
+        search(searchTerms);
+    }, 300);
+
+
+});
